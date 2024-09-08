6f0225a7deb20b5e60e27b5563e5c257f55a4f94
Steven Sanchez <iamstevensanchez04@gmail.com>
2024-09-07 21:04:34 -0500

    Added popup user login/signup menu on homepage

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
+from markupsafe import escape
+from flask import request
 
 app = Flask(__name__)
-
+#          <name> --> url variables
 @app.route("/")
 def home():
+    #User imported vaules need escape() to avoid injection attack jinja does automaically
     return render_template('index.html')
 
 if __name__ == '__main__':
