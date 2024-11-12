bb9d1bd36c5ba8d960defd3d484d92c862d9503c
steven sanchez <iamstevensanchez04@gmail.com>
2024-11-11 21:54:06 -0600

    Renedered the pie chart, still need resizing

--- flaskr/vis.py
+++ flaskr/vis.py
@@ -1,7 +1,7 @@
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
 )
 
 from flaskr.db import get_db
@@ -15,6 +15,7 @@ def display_graphs():
     user = session.get('user_id')
 
     # Query for classes and logged hours.
+    # studt_data is going to be used to hold all data about user study, hours,weeks,and course
     study_data = {}
 
     # Get the list of courses for the user
@@ -33,6 +34,7 @@ def display_graphs():
     ).fetchall()
 
     # Get the weekly logged hours for each course
+    # Storing into weekly_hours a tuple
     weekly_hours = db.execute(
         'SELECT course_name, strftime("%W", log_date) as week, SUM(hours) as weekly_hours '
         'FROM logged_hours '
@@ -43,10 +45,22 @@ def display_graphs():
     ).fetchall()
 
     # Prepare the data to be passed to the template
+    # Prepare the data to be passed to the template
     study_data['classes_data'] = [row['course_name'] for row in courses]  # Course names
     study_data['hours_data'] = [row['total_hours'] for row in course_hours]  # Total hours per course
     study_data['weeks_data'] = sorted(set([f"Week {int(row['week']) + 1}" for row in weekly_hours]))  # Unique weeks
-    study_data['study_hours_data'] = [row['weekly_hours'] for row in weekly_hours]  # Hours per week
 
-    # Render the template with the dynamic data
+    # This creates a dictionary where each course maps to a list of dictionaries with week and weekly_hours
+    study_data['study_hours_data'] = {}
+    for row in weekly_hours:
+        course_name = row['course_name']
+        week_data = {'week': row['week'], 'weekly_hours': row['weekly_hours']}
+        if course_name not in study_data['study_hours_data']:
+            study_data['study_hours_data'][course_name] = []
+        study_data['study_hours_data'][course_name].append(week_data)
+
+
+    
+    # Render the template with the html
+    # Returns study_data as dict
     return render_template('dash/visualize.html', study_data=study_data)
