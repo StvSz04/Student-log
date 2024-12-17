a9908ef8be056515dd9f3e7bacdbd923ee714ee4
steven sanchez <iamstevensanchez04@gmail.com>
2024-12-17 16:46:24 -0600

    Began adding entry deletion table

--- flaskr/delete_course.py
+++ flaskr/delete_course.py
@@ -11,14 +11,16 @@ import sqlite3
 
 bp = Blueprint('del', __name__, url_prefix='/delete_course')
 
-@bp.route('/delete', methods=('GET', 'POST'))
+@bp.route('/delete', methods=('GET','POST'))
 def delete_course():
     db = get_db()
     user_id = session.get('user_id')
     courses = db.execute("SELECT course_name FROM course WHERE user_username = ?", (user_id,)).fetchall()
+    entries = []
 
     if request.method == 'POST':
          course_to_remove = request.form.get('course') 
+         course_entry = request.form.get('course-entry')
          if course_to_remove:
             if not course_to_remove.strip():
                 error = 'Course name is required.'
@@ -39,6 +41,14 @@ def delete_course():
                     error = f"Course {course_to_remove} does not exist."
             #Updates the input into the
             return redirect(url_for('del.delete_course'))
+         if course_entry:
+            entries = db.execute("SELECT course_name, hours, log_date FROM logged_hours WHERE user_username = ? AND course_name = ?"
+            , (user_id, course_entry)).fetchall()
+            return redirect(url_for('del.delete_course'))
+         
+                
+
          
          
-    return render_template('dash/delete_course.html', courses=courses)
+    return render_template('dash/delete_course.html', courses=courses, entries=entries )
+
