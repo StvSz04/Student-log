22737631659a9cd3feb5bfbad1e3b1de63212ce6
steven sanchez <iamstevensanchez04@gmail.com>
2024-12-20 15:16:39 -0600

    Entry deletion table is now finished

--- flaskr/delete_course.py
+++ flaskr/delete_course.py
@@ -42,8 +42,28 @@ def delete_course():
          
         course_entry = request.form.get('course_for_entries')
         if course_entry:
-            entries = db.execute("SELECT hours, log_date FROM logged_hours WHERE user_username = ? AND course_name = ?",(user_id, course_entry)).fetchall()
+            entries = db.execute(
+                "SELECT hours, log_date FROM logged_hours WHERE user_username = ? AND course_name = ? ORDER BY log_date ASC",
+                (user_id, course_entry)
+            ).fetchall()
+
             return render_template('dash/delete_course.html', entries=entries, courses=courses, course_entry=course_entry)
+                 
+        entries_to_delete = request.form.getlist('entries_to_delete') 
+        if entries_to_delete:
+            #Delete needed data within the database an instantiate entries again
+            for entry in entries_to_delete:
+                course_name, hours, log_date = entry.split(',')
                 
+                # Delete each entry from the database
+                db.execute(
+                    "DELETE FROM logged_hours WHERE user_username = ? AND course_name = ? AND hours = ? AND log_date = ?",
+                    (user_id, course_name, hours, log_date)
+                )
+                db.commit()
+
+            return render_template('dash/delete_course.html', courses=courses)
+    
+    
     return render_template('dash/delete_course.html', courses=courses)
 
