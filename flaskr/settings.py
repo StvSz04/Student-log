72f7993f890ea4ff1406c74811b379677f94ee86
steven sanchez <iamstevensanchez04@gmail.com>
2024-12-20 23:12:39 -0600

    Changed css styles: colors,links,and nav bar

---
+++ flaskr/settings.py
@@ -0,0 +1,11 @@
+import functools
+
+from datetime import datetime
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from flaskr.db import get_db
+import sqlite3
+
+bp = Blueprint('setting', __name__, url_prefix='/setting')
